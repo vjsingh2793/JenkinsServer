47f55ff095256abfc4264ca41abc5fe91bd9494d
Vijay Singh <vjsingh2793@gmail.com>
2019-02-05 15:53:39 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# JenkinsServer
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: JenkinsServer
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
