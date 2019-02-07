e35e768924665422f284606fb5c3e7768f881aad
Vijay Singh <vjsingh2793@gmail.com>
2019-02-07 16:01:22 +0530

    Commiting metadta.rb

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,8 @@ supports CentOS
 version '0.1.0'
 chef_version '>= 13.0'
 depends 'firewall', '> 2.0'
+issues_url 'https://github.com/vjsingh2793/JenkinsServer/issues'
+source_url 'https://github.com/vjsingh2793/JenkinsServer'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
